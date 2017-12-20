e6427ef030be1f0e6ca96ec3d96971bbb8726de5
David Delavennat <david.delavennat@polytechnique.edu>
2017-12-20 16:33:56 +0100

    wip

--- patch.sh
+++ patch.sh
@@ -3,7 +3,7 @@ sed -i '/#define ETH_P_AOE/a \
 cat ipxe/src/include/ipxe/if_ether.h
 sed -i '/#define ERRFILE_httpntlm/a \
 #define ERRFILE_lldp      ( ERRFILE_NET | 0x004b0000 )'               ipxe/src/include/ipxe/errfile.h
-sed -i '/#define	NET_PROTO_LACP/a \
+#sed -i '/#define	NET_PROTO_LACP/a \
 #define NET_PROTO_LLDP          /* LLDP protocol */'                 ipxe/src/config/general.h
 cat ipxe/src/config/general.h
 sed -i '$ a \
@@ -15,3 +15,9 @@ cat ipxe/src/include/ipxe/settings.h
 sed -i '/#define DHCP_EB_FEATURE_NFS/a \
 #define DHCP_EB_FEATURE_LLDP 0x30 /**< LLDP protocol */'              ipxe/src/include/ipxe/features.h
 cat ipxe/src/include/ipxe/features.h
+
+echo "#define NET_PROTO_LLDP        /* LLDP protocol */'"   >> ipxe/src/config/local/general.h
+echo "#define LLDP_CMD              /* LLDP commands */"    >> ipxe/src/config/local/general.h
+echo "#define VLAN_CMD              /* VLAN commands */"    >> ipxe/src/config/local/general.h
+echo "#define PING_CMD              /* Ping command */"     >> ipxe/src/config/local/general.h
+echo "#define PCI_CMD               /* PCI commands */"     >> ipxe/src/config/local/general.h
