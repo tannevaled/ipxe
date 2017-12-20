41a17820872b80d2465f1847744d6d9f1534550a
David Delavennat <david.delavennat@polytechnique.edu>
2017-12-20 13:54:01 +0100

    wip

--- src/net/lldp.c
+++ src/net/lldp.c
@@ -319,8 +319,6 @@ static int lldp_rx ( struct io_buffer *iobuf, struct net_device *netdev,
 	uint16_t *tlv_header;
 	int rc = 0;
 
-  printf("lldp_rx!");
-
 	lldp_settings = zalloc ( sizeof ( struct lldp_settings ) +
 	                         2 * ( 2 + ll_protocol->ll_addr_len ) +
 	                         iob_len ( iobuf ) );
@@ -358,7 +356,8 @@ static int lldp_rx ( struct io_buffer *iobuf, struct net_device *netdev,
 
 	ref_put ( &lldp_settings->refcnt );
 
-  DBGC ( lldp_settings, "lldp_rx");
+  DBGC ( lldp_settings, "Got an LLDP packet from %010x\n"
+         ll_source );
 done:
 	free_iob ( iobuf );
 	return rc;
