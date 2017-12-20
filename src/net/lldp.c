0414b9bdc14d150c21a1b02a3c77ad49eb0ce385
David Delavennat <david.delavennat@polytechnique.edu>
2017-12-20 14:27:11 +0100

    debug

--- src/net/lldp.c
+++ src/net/lldp.c
@@ -356,8 +356,8 @@ static int lldp_rx ( struct io_buffer *iobuf, struct net_device *netdev,
 
 	ref_put ( &lldp_settings->refcnt );
 
-  DBGC ( lldp_settings, "Got an LLDP packet from %010x\n",
-         ll_source );
+  //DBGC ( lldp_settings, "Got an LLDP packet from %010x\n",
+  //       ll_source );
 done:
 	free_iob ( iobuf );
 	return rc;
