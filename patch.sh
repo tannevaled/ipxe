6c74a2472b865a2aa77d42db6711baa257186b3a
David Delavennat <david.delavennat@polytechnique.edu>
2017-12-20 12:49:21 +0100

    wip

--- patch.sh
+++ patch.sh
@@ -1,10 +1,17 @@
-sed -i '/#define ETH_P_AOE/a #define ETH_P_LLDP 0x88CC  /* Link Layer Discovery Protocol */'    ipxe/src/include/ipxe/if_ether.h
+sed -i '/#define ETH_P_AOE/a \
+#define ETH_P_LLDP      0x88CC   /* Link Layer Discovery Protocol */' ipxe/src/include/ipxe/if_ether.h
 cat ipxe/src/include/ipxe/if_ether.h
-sed -i '/#define ERRFILE_httpntlm/a #define ERRFILE_lldp      ( ERRFILE_NET | 0x004b0000 )'     ipxe/src/include/ipxe/errfile.h
-sed -i '/#define	NET_PROTO_LACP/a #define  NET_PROTO_LLDP          /* LLDP protocol */'        ipxe/src/config/general.h
+sed -i '/#define ERRFILE_httpntlm/a \
+#define ERRFILE_lldp      ( ERRFILE_NET | 0x004b0000 )'               ipxe/src/include/ipxe/errfile.h
+sed -i '/#define	NET_PROTO_LACP/a \
+#define NET_PROTO_LLDP          /* LLDP protocol */'                 ipxe/src/config/general.h
 cat ipxe/src/config/general.h
-sed -i '$ a #ifdef NET_PROTO_LLDP\nREQUIRE_OBJECT ( lldp );\n#endif'                            ipxe/src/config/config_ethernet.c
+sed -i '$ a \
+#ifdef NET_PROTO_LLDP\nREQUIRE_OBJECT ( lldp );\n#endif'              ipxe/src/config/config_ethernet.c
 cat ipxe/src/config/config_ethernet.c
-sed -i '/#define SETTING_MISC/a #define SETTING_LLDP               19 /**< LLDP settings */'    ipxe/src/include/ipxe/settings.h
+sed -i '/#define SETTING_MISC/a \
+#define SETTING_LLDP               19 /**< LLDP settings */'          ipxe/src/include/ipxe/settings.h
 cat ipxe/src/include/ipxe/settings.h
-sed -i '/#define DHCP_EB_FEATURE_NFS/a #define DHCP_EB_FEATURE_LLDP 0x30 /**< LLDP protocol */' ipxe/src/include/ipxe/features.h
+sed -i '/#define DHCP_EB_FEATURE_NFS/a \
+#define DHCP_EB_FEATURE_LLDP 0x30 /**< LLDP protocol */'              ipxe/src/include/ipxe/features.h
+cat ipxe/src/include/ipxe/features.h
